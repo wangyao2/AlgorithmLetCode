b1599db62584060fe5a2aa0167c0820384c9c142
wangyao2 <1910425@tongji.edu.cn>
2023-08-05 00:36:32 +0800

    新的分支处理问题1

---
+++ src/TreeRelative/issue_1.java
@@ -0,0 +1,14 @@
+package TreeRelative;
+
+/**
+ * ClassName:issue_1
+ * Description:
+ *
+ * @Create:2023/8/5 -0:35
+ */
+public class issue_1 {
+    public static void main(String[] args) {
+
+        System.out.println("处理问题1");
+    }
+}
