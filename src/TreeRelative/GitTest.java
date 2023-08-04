5832e1e9d2c3db75c48a7237912a733baadffa85
wangyao2 <1910425@tongji.edu.cn>
2023-08-04 21:34:58 +0800

    在hotfix第3次提交

--- src/TreeRelative/GitTest.java
+++ src/TreeRelative/GitTest.java
@@ -12,5 +12,11 @@ public class GitTest {
         System.out.println("在hotfix分支上面添加了新的一行代码");
         System.out.println("在hotfix分支上面添加了新的一行代码");
         System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
+
+        System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
+        System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
+        System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
+        System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
+        System.out.println("在hotfix分支上面添加了用于冲突合并的代码");
     }
 }
